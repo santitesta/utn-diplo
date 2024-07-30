f278958e04fc7a8e6ff3498516da68982ed56737
ezequielgalvan1985 <ezgatrabajo@gmail.com>
2024-07-30 18:02:10 -0300

    web3js.tutorial.1

---
+++ web3js/SimpleAuction/compile.js
@@ -0,0 +1,59 @@
+const solc = require("solc");
+const path = require("path");
+const fs = require("fs");
+
+
+const contractName = process.argv[2]; 
+
+const fileName = `${contractName}.sol`;
+const contractNameByteCode = contractName + "Bytecode.bin";
+const contractNameAbi = contractName + "Abi.json";
+
+console.log("filename:",fileName);
+console.log("dirname:",__dirname);
+
+// Read the Solidity source code from the file system
+const contractPath = path.join(__dirname, fileName);
+const sourceCode = fs.readFileSync(contractPath, "utf8");
+
+// solc compiler config
+const input = {
+  language: "Solidity",
+  sources: {
+    [fileName]: {
+      content: sourceCode,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+// Compile the Solidity code using solc
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log("compiledCode: ",compiledCode);
+
+// Get the bytecode from the compiled contract
+const bytecode =
+  compiledCode.contracts[fileName][contractName].evm.bytecode.object;
+
+// Write the bytecode to a new file
+const bytecodePath = path.join(__dirname, contractNameByteCode);
+fs.writeFileSync(bytecodePath, bytecode);
+
+// Log the compiled contract code to the console
+console.log("Contract Bytecode:\n", bytecode);
+
+// Get the ABI from the compiled contract
+const abi = compiledCode.contracts[fileName][contractName].abi;
+
+// Write the Contract ABI to a new file
+const abiPath = path.join(__dirname, contractNameAbi);
+fs.writeFileSync(abiPath, JSON.stringify(abi, null, "\t"));
+
+// Log the Contract ABI to the console
+console.log("Contract ABI:\n", abi);
