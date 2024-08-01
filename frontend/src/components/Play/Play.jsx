cd3f3545c198688d4c579df7bffab01812f67704
Enzo Meoniz <emeoniz@na-sa.com.ar>
2024-08-01 14:53:04 -0300

    Trabajo en una botonera en la seccion de play del frontend

--- frontend/src/components/Play/Play.jsx
+++ frontend/src/components/Play/Play.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 
 import BannerPozo from "../BannerPozo";
+import Botonera from "./Botonera";
 
 function Play() {
-
+  
   return (
     <div>
       <Container fluid className="resume-section">
         <BannerPozo />
+        <Botonera />
       </Container>
     </div>
   );
